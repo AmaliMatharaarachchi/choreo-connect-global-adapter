579bf83603db8600c1388bf2ac7488a70dd567f6
malithmunasinghe <mpmunasinghe@gmail.com>
2021-07-21 09:30:04 +0530

    Adding updated test

--- global-adapter/internal/synchronizer/apis_fetcher_test.go
+++ global-adapter/internal/synchronizer/apis_fetcher_test.go
@@ -95,6 +95,9 @@ func TestAddAPIEventsToChannel(t *testing.T) {
 							Vhost: "localhost",
 						},
 					},
+					Version: "1.0.0",
+					APIContext: "/testorg/myorg/v1",
+
 				},
 				{
 					APIFile: "50d2cda9e9cab41a3244fa91-70d2cdade9cab41a3214fa98",
@@ -104,6 +107,8 @@ func TestAddAPIEventsToChannel(t *testing.T) {
 							Vhost: "localhost",
 						},
 					},
+					Version: "1.0.0",
+					APIContext: "/testorg/myorg/v1",
 				},
 			},
 		},
