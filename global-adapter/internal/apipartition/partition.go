91da23727016aabc22eabb994e59e07c11f4ebc7
VajiraPrabuddhaka <vajiraprabuddhaka@gmail.com>
2022-03-24 13:20:42 +0530

    fix review suggestions: - do the readiness health true, in the PopulateAPIData

--- global-adapter/internal/apipartition/partition.go
+++ global-adapter/internal/apipartition/partition.go
@@ -141,42 +141,31 @@ func PopulateAPIData(apiEventsWithStartupFlag synchronizer.APIEventsWithStartupF
 		}
 		logger.LoggerAPIPartition.Infof("Cache keys were successfully updated into redis cache at the startup(y/n) : %v", apiEventsWithStartupFlag.IsStartup)
 		cache.PublishUpdatedAPIKeys(cacheObj, rc)
-		pushToXdsCache(laAPIList, apiEventsWithStartupFlag.IsStartup)
+		pushToXdsCache(laAPIList)
+		if apiEventsWithStartupFlag.IsStartup {
+			logger.LoggerAPIPartition.Info("All artifacts have been loaded to XDS cache in the startup. Hense marking readiness as true")
+			health.Startup.SetStatus(true)
+		}
 	} else {
-		pushToXdsCache(laAPIList, apiEventsWithStartupFlag.IsStartup)
+		pushToXdsCache(laAPIList)
+		if apiEventsWithStartupFlag.IsStartup {
+			logger.LoggerAPIPartition.Info("All artifacts have been loaded to XDS cache in the startup. Hense marking readiness as true")
+			health.Startup.SetStatus(true)
+		}
 	}
 }
 
-func pushToXdsCache(laAPIList []*types.LaAPIEvent, isStartup bool) {
+func pushToXdsCache(laAPIList []*types.LaAPIEvent) {
 	logger.LoggerAPIPartition.Debug("API List : ", len(laAPIList))
-	if isStartup {
-		switch n := len(laAPIList); {
-		case n == 0:
-			logger.LoggerAPIPartition.Info("No artifacts has been loaded to XDS cache in the startup. Anyway marking readiness as true")
-			health.Startup.SetStatus(true)
-			return
-		case n == 1:
-			xds.ProcessSingleEvent(laAPIList[0])
-			logger.LoggerAPIPartition.Info("All artifacts have been loaded to XDS cache in the startup. Hense marking readiness as true")
-			health.Startup.SetStatus(true)
-			return
-		default:
-			xds.AddMultipleAPIs(laAPIList)
-			logger.LoggerAPIPartition.Info("All artifacts have been loaded to XDS cache in the startup. Hense marking readiness as true")
-			health.Startup.SetStatus(true)
-			return
-		}
-	} else {
-		switch n := len(laAPIList); {
-		case n == 0:
-			return
-		case n == 1:
-			xds.ProcessSingleEvent(laAPIList[0])
-			return
-		default:
-			xds.AddMultipleAPIs(laAPIList)
-			return
-		}
+	switch n := len(laAPIList); {
+	case n == 0:
+		return
+	case n == 1:
+		xds.ProcessSingleEvent(laAPIList[0])
+		return
+	default:
+		xds.AddMultipleAPIs(laAPIList)
+		return
 	}
 }
 
@@ -396,7 +385,7 @@ func DeleteAPIRecord(api *synchronizer.APIEvent, laLabels map[string]map[string]
 					IsRemoveEvent:    true,
 					OrganizationUUID: api.OrganizationID,
 					LabelHierarchy:   gatewayLabel,
-				}}, false)
+				}})
 			}
 		}
 	} else {
